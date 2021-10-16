012f3fec46e5f6e87da3063866a9b33328e020f2
Levi Wilkerson <chelleas123@gmail.com>
2021-10-16 13:25:44 -0400

    added firebase app authorization

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB6ILViLUrFNIqs2rF6X-NOEpwYsyhOPcg",
+  authDomain: "calorie-counter-f457f.firebaseapp.com",
+  projectId: "calorie-counter-f457f",
+  storageBucket: "calorie-counter-f457f.appspot.com",
+  messagingSenderId: "199242851087",
+  appId: "1:199242851087:web:576a4a73c17e57f57b547a",
+  measurementId: "G-2TCMGFJBCH",
+};
+
+export default firebase.initializeApp(firebaseConfig);
