9ab2b7068f29159629a46e15d648ca79905b935b
Levi Wilkerson <chelleas123@gmail.com>
2021-10-22 20:35:25 -0400

    addded made with love styling

--- src/components/MadeWithLove.js
+++ src/components/MadeWithLove.js
@@ -2,17 +2,15 @@ import React from "react";
 
 export default function MadeWithLove() {
   return (
-    <div className="footer-madeWithLove">
-      <div>
-        Made with <i className="fas fa-heart"></i> by Levi
-      </div>
+    <div className="madeWithLove">
       <div>
         <a
           href="https://github.com/leviFrosty/twitClone"
           target="_blank"
           rel="noreferrer"
+          className="madeWithLove__github"
         >
-          See the code: <i className="fab fa-github"></i>
+          See the code <i className="fab fa-github"></i>
         </a>
       </div>
     </div>
